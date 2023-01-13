04b969a5dfe40c4dd0d9b9b1efe7b27dc1b26e9c
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-13 22:57:27 +0300

    first task done

--- 13.01.1/Program.cs
+++ 13.01.1/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int max = 0;
+int min = 0;
+
+System.Console.WriteLine("Введите первое целое число ");
+
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе целое число ");
+
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) {
+    max = a;
+    min = b;
+}
+else {
+    max = b;
+    min = a;
+};
+System.Console.WriteLine("Максимальное число " + max);
+System.Console.WriteLine("Минимальное число "+ min);
