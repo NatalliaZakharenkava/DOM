16f396d11c8bd468c25c8b64500377b23701c3c4
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-16 13:59:54 +0300

    fourth task

--- 13.01.4/Program.cs
+++ 13.01.4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите целое число ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int a = 2;
+ if (N > 1) {
+    while (a <= N) {
+        System.Console.Write(a + " ");
+        a = a + 2;
+    }
+}
