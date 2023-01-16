fd2fd8ee4719939ccf9b8b794d57cfab8efe718e
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-16 13:38:51 +0300

    third task

--- 13.01.3/Program.cs
+++ 13.01.3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите целое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) {
+    System.Console.WriteLine("Ваше число четное");
+}
+else {
+    System.Console.WriteLine("Ваше число нечетное");
+}
+
