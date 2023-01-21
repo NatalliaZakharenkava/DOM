3a04902520ad1c1d2c7a9e6db527d146ca2cd1df
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-21 18:27:35 +0300

    task 19 done

--- 19.01.19/Program.cs
+++ 19.01.19/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿System.Console.WriteLine("--> ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 10000;
+
+
+int c = a % 10;
+
+if (a > 10000 && a < 100000) {
+
+if (b == c) {
+    System.Console.WriteLine("Палиндром");
+}
+else {
+    System.Console.WriteLine("Не палиндром");
+}
+ }
+
+ else {
+    System.Console.WriteLine("Введите пятизначное число ");
+ };
+ 
+
