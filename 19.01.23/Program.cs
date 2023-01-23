b3d15d7ce8491c0ad12334947ceafcd5d4d76955
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-23 11:43:24 +0300

    add coment

--- 19.01.23/Program.cs
+++ 19.01.23/Program.cs
@@ -1,4 +1,6 @@
-﻿System.Console.WriteLine("--> ");
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("--> ");
 
 int n = Convert.ToInt32(Console.ReadLine());
 
