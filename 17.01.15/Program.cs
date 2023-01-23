01ea9912a394aee1feede2542a0c444621f172b3
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-23 11:05:20 +0300

    task 15 done

--- 17.01.15/Program.cs
+++ 17.01.15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите цифру от 1 до 7 ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 1 && n <= 5) {
+    System.Console.WriteLine("Будний день!");
+}
+else if (n >= 6 && n <= 7) {
+    System.Console.WriteLine("Выходной!");
+}
+else {
+    System.Console.WriteLine("Неверная цифра, попробуйте снова");
+};
+
+
